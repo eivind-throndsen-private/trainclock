23015d9ad93caab8ddf4ec8943337c340fef2654
Eivind Throndsen <eivind.throndsen@gmail.com>
2024-12-04 22:25:18 +0100

    Cache versioning and button position fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'swiss-clock-v1';
+const CACHE_VERSION = '2';
+const CACHE_NAME = `swiss-clock-v${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/ch-clock/',
   '/ch-clock/index.html',
@@ -20,9 +21,34 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseToCache = networkResponse.clone();
+          
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => response);
+    })
   );
 });
